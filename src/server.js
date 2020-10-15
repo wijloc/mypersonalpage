4e981a0a6c3dbbeabd2d73bb34565e69f7ddb5ff
Willian Oliveira <wijloc@gmail.com>
2020-10-15 18:03:20 -0300

    Criação do esqueleto da página Preenchimento de algumas informações

---
+++ src/server.js
@@ -0,0 +1,21 @@
+//importar dependencias
+const express = require("express");
+const path = require("path");
+const pages = require("./pages.js");
+
+//iniciando o express
+const server = express();
+
+server
+  //utilizando os arquivos estásticos
+  .use(express.static("public"))
+
+  //configurar template engine
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "hbs")
+
+//criar rotas da aplicacao
+.get("/", pages.index)
+
+//ligar o servidor
+server.listen(5500);
